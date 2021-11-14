40ff6d329ea2456a1ce05c95cfe97cd0d456b30f
rizkiakbar2302@gmail.com <rizkiakbar2302@gmail.com>
2021-11-14 22:24:31 +0700

    db,konek,proses,login_form

---
+++ Proses.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once 'config/Koneksi.php';
+
+class Proses extends Koneksi
+{
+    public function loginPetugas($username, $password)
+    {
+        $stmt =  mysqli_query($this->konek, "SELECT * FROM tb_petugas WHERE username = '" . $username . "' AND password = '" . $password . "' ");
+
+        if ($stmt->num_rows > 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
+$proses = new Proses;
+var_dump($proses->loginPetugas('admin',  sha1('admin')));
---
+++ config/Koneksi.php
@@ -0,0 +1,21 @@
+<?php
+
+class Koneksi
+{
+    var $host = 'localhost';
+    var $user = 'root';
+    var $pass = '';
+    var $db_name = 'spp';
+
+    public function __construct()
+    {
+        $this->konek = mysqli_connect($this->host, $this->user, $this->pass, $this->db_name);
+
+        if ($this->konek) {
+            echo "Koneksi Sukses";
+        } else {
+            echo "Koneksi Gagal";
+        }
+    }
+}
+// $konek = new Koneksi;
