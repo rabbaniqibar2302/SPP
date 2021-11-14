6c42e78c05c9f0786e2175d77c4774c79811eb8c
rizkiakbar2302@gmail.com <rizkiakbar2302@gmail.com>
2021-11-14 22:03:02 +0700

    add index

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aplikasi Pembayaran SPP</title>
+</head>
+<body>
+    <center>
+        <h4>Silahkan Masuk</h4>
+        <form action="" method="post">
+            <label for="username">Username</label>
+            <input type="text" name="username" id="username"><br>
+            <label for="password">Password</label>
+            <input type="password" name="password" id="password"><br>
+            <input type="submit" value="Masuk">
+        </form>
+    </center>
+</body>
+</html>
